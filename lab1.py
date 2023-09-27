74182b5f2d42323ceae6c30b6f62896aea7db649
Francisco Fonseca <35011119+franfrancisco9@users.noreply.github.com>
2023-09-27 09:34:41 +0000

    test k

--- lab1.py
+++ lab1.py
@@ -26,6 +26,8 @@ def press_statistic(y_true, y_pred, xs):
     res = y_pred - y_true
     hat = xs.dot(np.linalg.pinv(xs))
     den=(1-np.diagonal(hat))
+    if np.any(den<=0):
+        return np.inf
     sqr = np.square(res/den)
     return sqr.sum()
 
@@ -55,7 +57,7 @@ X_test_final = np.load("X_test_regression1.npy")
 # Split
 # Seems to not make sense to split the data
 # Since we have a small dataset, we will not split the data
-# X_train, X_test, Y_train, Y_test = train_test_split(X_train, Y_train, test_size=0.01, random_state=95789)
+# X_train, X_test, Y_train, Y_test = train_test_split(X_train, Y_train, test_size=0.01)
 
 # # Lasso
 # print("Lasso")
@@ -180,7 +182,7 @@ print("======================LinearRegression======================")
 best_SSE = np.inf
 best_R2_Pred = -np.inf
 for k in range(3, 6):
-    kf = KFold(n_splits=k, shuffle=True, random_state=95789)
+    kf = KFold(n_splits=k, shuffle=True)
     for train_index, test_index in kf.split(X_train):
         # print("K:", k)
         # print(len(list(kf.split(X_train))))
@@ -210,7 +212,7 @@ print("======================Lasso======================")
 best_SSE = np.inf
 best_R2_Pred = -np.inf
 for k in range(3, 6):
-    kf = KFold(n_splits=k, shuffle=True, random_state=95789)
+    kf = KFold(n_splits=k, shuffle=True)
     for train_index, test_index in kf.split(X_train):
         # print("K:", k)
         # print(len(list(kf.split(X_train))))
@@ -242,33 +244,38 @@ for k in range(3, 6):
 print("======================Ridge======================")
 best_SSE = np.inf
 best_R2_Pred = -np.inf
-for k in range(3, 6):
-    kf = KFold(n_splits=k, shuffle=True, random_state=95789)
-    for train_index, test_index in kf.split(X_train):
-        # print("K:", k)
-        # print(len(list(kf.split(X_train))))
-        # print(list(kf.split(X_train)))
-        X_train_cv, X_test_cv = X_train[train_index], X_train[test_index]
-        Y_train_cv, Y_test_cv = Y_train[train_index], Y_train[test_index]
-        regressor = Ridge(
-            random_state=95789,
-            alpha = 0.05
-        )
-        regressor.fit(X_train_cv, Y_train_cv)
-        Y_pred_test = regressor.predict(X_test_cv)
-        Y_pred = regressor.predict(X_train_cv)
-        SSE = 0
-        for i in range(len(Y_test_cv)):
-            SSE += (Y_test_cv[i] - Y_pred_test[i])**2
-        if SSE[0] < best_SSE :
-            print("Found better SSE")
-            print("K:", k)
-            print("train_index:", train_index)
-            print("test_index:", test_index)
-            print("SSE:", SSE[0])
-            print("R2:", r2_score(Y_train_cv, Y_pred))
-            print("R2 adj:",adj_r2_score(15, 10, r2_score(Y_train_cv, Y_pred)))
-            print("R2 pred:",predicted_r2(Y_train_cv, Y_pred, X_train_cv))
-            best_SSE = SSE[0]
+for _ in range(1000):
+    for k in range(3, 6):
+        kf = KFold(n_splits=k, shuffle=True)
+        for train_index, test_index in kf.split(X_train):
+            # test alpha from 0.01 up to 10 giong 0.01
+            for alpha in np.linspace(0.01, 10, 1000):
+                # print("K:", k)
+                # print(len(list(kf.split(X_train))))
+                # print(list(kf.split(X_train)))
+                X_train_cv, X_test_cv = X_train[train_index], X_train[test_index]
+                Y_train_cv, Y_test_cv = Y_train[train_index], Y_train[test_index]
+                regressor = Ridge(
+                    random_state=95789,
+                    alpha = alpha
+                )
+                regressor.fit(X_train_cv, Y_train_cv)
+                Y_pred_test = regressor.predict(X_test_cv)
+                Y_pred = regressor.predict(X_train_cv)
+                SSE = 0
+                for i in range(len(Y_test_cv)):
+                    SSE += (Y_test_cv[i] - Y_pred_test[i])**2
+                if SSE[0] < best_SSE and predicted_r2(Y_train_cv, Y_pred, X_train_cv) > best_R2_Pred:
+                    print("Found better SSE")
+                    print("K:", k)
+                    print("train_index:", train_index)
+                    print("test_index:", test_index)
+                    print("Alpha:", alpha)
+                    print("SSE:", SSE[0])
+                    print("R2:", r2_score(Y_train_cv, Y_pred))
+                    print("R2 adj:",adj_r2_score(15, 10, r2_score(Y_train_cv, Y_pred)))
+                    print("R2 pred:",predicted_r2(Y_train_cv, Y_pred, X_train_cv))
+                    best_SSE = SSE[0]
+                    best_R2_Pred = predicted_r2(Y_train_cv, Y_pred, X_train_cv)
 
 
