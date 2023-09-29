9e2dab75e8b5709a79b8c9e2df79a42d6aff0e27
franfrancisco9 <francisco.parreira.fonseca@gmail.com>
2023-09-29 18:58:34 +0100

    clean final

--- final.py
+++ final.py
@@ -35,10 +35,6 @@ def Linear_ordinary_regression(x_train, y_train, x_test):
     return (y_pred, SSE, y_test)
 
 
-# order - order of the polynomial
-# x_train - feature vector
-# y_train - vector of outcomes
-# x_test - feature vector for testing and evaluating the used model
 def polynomial_regression(order, x_train, y_train, x_test):
     """
     Function that performs polynomial regression.
@@ -251,31 +247,29 @@ y_train = np.load('Y_train_regression1.npy')
 x_test = np.load('X_test_regression1.npy')
 
 order = 3 # order of the polynomial
-lambda_ridge = 0.999 # Regularization term for Ridge regression
-lambda_lasso = 0.096 # Regularization term for Lasso regression
+alpha_ridge = 1.89 # Regularization term for Ridge regression
+alpha_lasso = 0.096 # Regularization term for Lasso regression
 
 print("\nData shape of vector of features data and vector of outcomes data for training: ", x_train.shape, y_train.shape)
 print("\nData shape of vector of features for prediction: ", x_test.shape)
 
 # Obtain betas coefficients that best fit the models with the given data and calculate the respective SSE  
 ordinary_tuple = Linear_ordinary_regression(x_train, y_train, x_test)
-print('\nSSE for Ordinary regression is:', ordinary_tuple[1])
+print('\nSSE for Ordinary regression is:', ordinary_tuple[1][0])
 
 poly_tuple = polynomial_regression(order, x_train, y_train, x_test)
-print("\nSSE for Polynomial regression of order", order, "for the evaluation set is:", poly_tuple[1])
+print("\nSSE for Polynomial regression of order", order, "for the evaluation set is:", poly_tuple[1][0])
 
-ridge_tuple = ridge(lambda_ridge, x_train, y_train, x_test)
-print('\nSSE for Ridge regression with lambda =', lambda_ridge, 'is:', ridge_tuple[1])
+ridge_tuple = ridge(alpha_ridge, x_train, y_train, x_test)
+print('\nSSE for Ridge regression with Alpha =', alpha_ridge, 'is:', ridge_tuple[1][0])
 
-lasso_tuple = lasso(lambda_lasso, x_train, y_train, x_test)     
-print('\nSSE for Lasso regression with lambda =', lambda_lasso, 'is:', lasso_tuple[1])
+lasso_tuple = lasso(alpha_lasso, x_train, y_train, x_test)     
+print('\nSSE for Lasso regression with Alpha =', alpha_lasso, 'is:', lasso_tuple[1][0])
 
 
 # ------------------------------------------------------------------------------------------------------------------------------------ #
-# At this point we already know which model better minimizes the SSE and better adjusts to the training data.
-# But we cannot guarentee that this model is robust for new data, for example, the polynomial regression may suffer from overfitting.
-# For this reason, we are going to consider a third set, the validation set.
-# Separate known data into training set and validation set in order to check hyperparameters or overfiting, for example.
+# To test for overfitting we will now separate known data into training and validation set in order 
+# to check hyperparameters and/or overfiting.
 test_size = 0.3
 (x_train_new, x_validation, y_train_new, y_validation) = train_test_split(x_train, y_train, test_size=test_size, shuffle=False) 
 
@@ -291,58 +285,56 @@ y_poly_test = poly_tuple[2]
 SSE = (np.linalg.norm(y_validation - y_poly_test))**2
 print("\nSSE for Polynomial regression of order", order, "for the validation set is:", SSE)
 
-ridge_tuple = ridge(lambda_ridge, x_train_new, y_train_new, x_validation)
+ridge_tuple = ridge(alpha_ridge, x_train_new, y_train_new, x_validation)
 # Calculate SSE with the validation set
 y_ridge_test = ridge_tuple[2]
 SSE = (np.linalg.norm(y_validation - y_ridge_test))**2
-print("\nSSE for Ridge regression with lambda =", lambda_ridge, "for the validation set is:", SSE)
+print("\nSSE for Ridge regression with Alpha =", alpha_ridge, "for the validation set is:", SSE)
 
-lasso_tuple = lasso(lambda_lasso, x_train_new, y_train_new, x_validation)
+lasso_tuple = lasso(alpha_lasso, x_train_new, y_train_new, x_validation)
 # Calculate SSE with the validation set
 y_lasso_test = lasso_tuple[2]
 SSE = (np.linalg.norm(y_validation - y_lasso_test))**2
-print("\nSSE for Lasso regression with lambda =", lambda_lasso, "for the validation set is:", SSE)
+print("\nSSE for Lasso regression with Alpha =", alpha_lasso, "for the validation set is:", SSE)
 
-# -------------------------------------------------------------------------------------------------------------------------------------------------------- #
-# ------------------------------------------- Tunning hyperparameters for lasso and ridge (to justify the choices above) --------------------------------- #
-# -------------------------------------------------------------------------------------------------------------------------------------------------------- #
-# ------------------ Compare average SSE with cross-validation using different values of lambda for Ridge regression and Lasso regression ---------------- #
 
-# Validation for different test sizes and different lambdas:
-alpha_ridge = np.arange(0.001, 1, 0.001) # alpha array to tune for ridge
-alpha_lasso = np.arange(0.001, 1, 0.001) # alpha array to tune for lasso
-test_size_vector = np.arange(0.10, 0.50, 0.01)
+# Tunning hyperparameters for lasso and ridge which appear above
+# Average SSE comparison with cross-validation using different values of alpha for Ridge regression and Lasso regression 
+
+# Validation for different test sizes and different Alphas:
+alpha_ridge = np.arange(0.001, 10, 0.001) # alpha array to tune for ridge
+alpha_lasso = np.arange(0.001, 10, 0.001) # alpha array to tune for lasso
+test_size_vector = np.arange(0.065, 0.35, 0.01)
 SSE_split_ridge = np.zeros(len(alpha_ridge))
 SSE_split_lasso = np.zeros(len(alpha_lasso))
 SSE_temp = 0
-# for i in range(len(alpha_ridge)):
-#   for test_size in test_size_vector:
+for i in range(len(alpha_ridge)):
+  for test_size in test_size_vector:
 
-#       # Split Training set into new Training data and a Validation set
-#       (x_train_new, x_validation, y_train_new, y_validation) = train_test_split(x_train, y_train, test_size=test_size, shuffle=True, random_state=42) 
+      # Split Training set into new Training data and a Validation set
+      (x_train_new, x_validation, y_train_new, y_validation) = train_test_split(x_train, y_train, test_size=test_size, shuffle=True, random_state=42) 
 
-#       ridge_tuple = ridge(alpha_ridge[i], x_train_new, y_train_new, x_validation)
-#       # Calculate SSE with the validation set
-#       y_ridge_test = ridge_tuple[2]
-#       SSE_split_ridge[i] += (np.linalg.norm(y_validation - y_ridge_test))**2
+      ridge_tuple = ridge(alpha_ridge[i], x_train_new, y_train_new, x_validation)
+      # Calculate SSE with the validation set
+      y_ridge_test = ridge_tuple[2]
+      SSE_split_ridge[i] += (np.linalg.norm(y_validation - y_ridge_test))**2
 
-#   SSE_split_ridge[i] = SSE_split_ridge[i]/len(test_size_vector)
+  SSE_split_ridge[i] = SSE_split_ridge[i]/len(test_size_vector)
 
 
-# for i in range(len(alpha_lasso)):
-#   for test_size in test_size_vector:
+for i in range(len(alpha_lasso)):
+  for test_size in test_size_vector:
 
-#       # Split Training set into new Training data and a Validation set
-#       (x_train_new, x_validation, y_train_new, y_validation) = train_test_split(x_train, y_train, test_size=test_size, shuffle=True, random_state=42) 
+      # Split Training set into new Training data and a Validation set
+      (x_train_new, x_validation, y_train_new, y_validation) = train_test_split(x_train, y_train, test_size=test_size, shuffle=True, random_state=42) 
 
-#       lasso_tuple = lasso(alpha_lasso[i], x_train_new, y_train_new, x_validation)
-#       # Calculate SSE with the validation set
-#       y_lasso_test = lasso_tuple[2]
-#       SSE_split_lasso[i] += (np.linalg.norm(y_validation - y_lasso_test))**2
-#   SSE_split_lasso[i] = SSE_split_lasso[i]/len(test_size_vector)
+      lasso_tuple = lasso(alpha_lasso[i], x_train_new, y_train_new, x_validation)
+      # Calculate SSE with the validation set
+      y_lasso_test = lasso_tuple[2]
+      SSE_split_lasso[i] += (np.linalg.norm(y_validation - y_lasso_test))**2
+  SSE_split_lasso[i] = SSE_split_lasso[i]/len(test_size_vector)
 
-# -------------------------------------------- Validation using Cross-Validations with K = Kfolds -------------------------------------------------------- #
-
+# Validation using Cross-Validations with K = Kfolds 
 Kfolds = 5
 SSE_validation_ridge = np.zeros(len(alpha_ridge))
 SSE_validation_lasso = np.zeros(len(alpha_lasso))
@@ -391,16 +383,15 @@ for i in range(len(alpha_lasso)):
         best_alpha_lasso = alpha_lasso[i]
         SSE_min = SSE_validation_lasso[i]
 
-# ------------------------ Now, using Cross-validation for different values of lambda and random shuffling of data ----------------------------- #
-
+# Cross-validation for different values of alpha and random shuffling of data 
 SSE_cv_shuffle_ridge = np.zeros(len(alpha_ridge))
 SSE_cv_shuffle_lasso = np.zeros(len(alpha_lasso))
 
 for i in range(len(alpha_ridge)):
-  SSE_cv_shuffle_ridge[i]  = Kfolds_ridge_regression(Kfolds, x_train, y_train, alpha_ridge[i], 10)
+  SSE_cv_shuffle_ridge[i]  = Kfolds_ridge_regression(Kfolds, x_train, y_train, best_alpha_ridge, 100)
 
 for i in range(len(alpha_lasso)):
-  SSE_cv_shuffle_lasso[i]  = Kfolds_lasso_regression(Kfolds, x_train, y_train, alpha_lasso[i], 10)
+  SSE_cv_shuffle_lasso[i]  = Kfolds_lasso_regression(Kfolds, x_train, y_train, best_alpha_lasso, 100)
 
 # Plot results
 
@@ -408,7 +399,7 @@ plot_ridge = plt.figure(1)
 plt.plot(alpha_ridge, SSE_validation_ridge)
 
 # Plot axes labels and show the plot
-plt.xlabel('Lambda [Ridge]')
+plt.xlabel('Alpha [Ridge]')
 plt.ylabel('SSE')
 plt.title('Average SSE computed after Cross Validation with K = {} folds'.format(Kfolds))
 plt.draw()
@@ -417,7 +408,7 @@ plot_lasso = plt.figure(2)
 plt.plot(alpha_lasso, SSE_validation_lasso)
 
 # Plot axes labels and show the plot
-plt.xlabel('Lambda [Lasso]')
+plt.xlabel('Alpha [Lasso]')
 plt.ylabel('SSE')
 plt.title('Average SSE computed after Cross Validation with K = {} folds'.format(Kfolds))
 plt.draw()
@@ -428,7 +419,7 @@ plot_ridge = plt.figure(7)
 plt.plot(alpha_ridge, SSE_validation_ridge)
 
 # Plot axes labels and show the plot
-plt.xlabel('Lambda')
+plt.xlabel('Alpha')
 plt.ylabel('SSE')
 plt.title('Ridge vs Lasso SSE computed after Cross Validation with K = {} folds'.format(Kfolds))
 plt.draw()
@@ -441,7 +432,7 @@ plot_lasso = plt.figure(3)
 plt.plot(alpha_ridge, SSE_cv_shuffle_ridge)
 
 # Plot axes labels and show the plot
-plt.xlabel('Lambda [Ridge]')
+plt.xlabel('Alpha [Ridge]')
 plt.ylabel('SSE')
 plt.title('Average SSE computed after Cross Validation with shuffling for K = {} folds'.format(Kfolds))
 plt.draw()
@@ -450,7 +441,7 @@ plot_lasso = plt.figure(4)
 plt.plot(alpha_lasso, SSE_cv_shuffle_lasso)
 
 # Plot axes labels and show the plot
-plt.xlabel('Lambda [Lasso]')
+plt.xlabel('Alpha [Lasso]')
 plt.ylabel('SSE')
 plt.title('Average SSE computed after Cross Validation with shuffling for K = {} folds'.format(Kfolds))
 plt.draw()
@@ -459,7 +450,7 @@ plot_lasso = plt.figure(5)
 plt.plot(alpha_ridge, SSE_split_ridge)
 
 # Plot axes labels and show the plot
-plt.xlabel('Lambda [Ridge]')
+plt.xlabel('Alpha [Ridge]')
 plt.ylabel('SSE')
 plt.title('Average SSE computed after Cross Validation with different test sizes')
 plt.draw()
@@ -468,25 +459,24 @@ plot_lasso = plt.figure(6)
 plt.plot(alpha_lasso, SSE_split_lasso)
 
 # Plot axes labels and show the plot
-plt.xlabel('Lambda [Lasso]')
+plt.xlabel('Alpha [Lasso]')
 plt.ylabel('SSE')
 plt.title('Average SSE computed after Cross Validation with different test sizes')
 plt.draw()
 
 # ------------------------------------------- Compare the models with tuned hyperparameters using cross-validation ------------------------------------------- #
 
-SSE_ord = Kfolds_linear_ordinary_regression(Kfolds, x_train, y_train, 43)
-SSE_ridge = Kfolds_ridge_regression(Kfolds, x_train, y_train, alpha_ridge[i], 43)
-SSE_lasso = Kfolds_lasso_regression(Kfolds, x_train, y_train, alpha_lasso[i], 43)
+SSE_ord = Kfolds_linear_ordinary_regression(Kfolds, x_train, y_train, 100)
+SSE_ridge = Kfolds_ridge_regression(Kfolds, x_train, y_train, best_alpha_ridge, 100)
+SSE_lasso = Kfolds_lasso_regression(Kfolds, x_train, y_train, best_alpha_lasso, 100)
 
 print('SSE ordinary:', SSE_ord, '\nSSE Ridge:', SSE_ridge, '\nSSE Lasso:', SSE_lasso)
 
+print("Best alpha for Ridge regression:", best_alpha_ridge)
+print("Best alpha for Lasso regression:", best_alpha_lasso)
 
+# Submision - Best model with tuned hyperparameters
 
-# SUBMISSION - FINAL TEST WITH BEST MODEL AND TUNED HYPERPARAMETERS 
-
-print(best_alpha_ridge)
-print(best_alpha_lasso)
 
 ridge_tuple = ridge(best_alpha_ridge, x_train, y_train, x_test)
 y_submit = ridge_tuple[2]
